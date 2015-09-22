8b9c81b5c674ce7f7848423ed329bf6fc3de005c
Vladimir Canic <vladimircanic@hotmail.com>
2015-09-22 14:05:48 +0200

    removed all necessary files

--- PythonProjects/api/pythonenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/vladimir/Documents/PraksaHtec/PythonProjects/api/pythonenv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
